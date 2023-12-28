8f0bf065c0f30a40862ef8265cd359313b33f325
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-28 16:36:40 +0100

    new

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add from '../Img/addAvatar.png'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../Firebase'
 
 const Register = () => {
+const [err,setErr] = useState(false)
+
+const handleClick = async (e) =>{
+  e.preventDefault() 
+   const displayName = e.target[0].value;
+   const email = e.target[1].value;
+   const password = e.target[2].value;
+   const file = e.target[3].files[0];
+
+
+  try{
+ const res = await createUserWithEmailAndPassword(auth, email, password)
+  }catch(err){
+    setErr(true);
+  }
+}
+
+
   return (
     <div className='formContainer'>
 
-    <div className='formWrapper'>
-    <span className='logo'>Lama Chat</span>
-    <span className='title'>Register</span>
-    <form>
-    <input type='text' placeholder="display name"/>
-    <input type='email' placeholder='email'/>
-    <input type='password' placeholder="password"/>
-    <input style={{display:'none'}} type='file'  id="file"/>
-    <label htmlFor='file'>
-    <img src={Add} alt=''/>
-    <span>Add an avatar</span></label>
-    <button>Sign up</button>
-    </form>
-    <p>do you have an account? Login</p>
-    
-    </div>
-      
+      <div className='formWrapper'>
+        <span className='logo'>Lama Chat</span>
+        <span className='title'>Register</span>
+
+        <form  onSubmit={handleClick}>
+          <input type='text' placeholder="display name" />
+          <input type='email' placeholder='email' />
+          <input type='password' placeholder="password" />
+          <input style={{ display: 'none' }} type='file' id="file" />
+          <label htmlFor='file'>
+            <img src={Add} alt='' />
+            <span>Add an avatar</span></label>
+          <button>Sign up</button>
+          {err && <span>Something Went Wrong !</span>}
+        </form>
+        <p>do you have an account? Login</p>
+
+      </div>
+
     </div>
   )
 }
