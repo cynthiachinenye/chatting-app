b55a71af6b33beec77c94bb0a8a36875163271f3
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-28 14:01:51 +0100

    new

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import Cam from '../Img/cam.png'
+import Add from '../Img/add.png'
+import More from '../Img/more.png'
+import Messages from './Messages'
+import Input from './Input'
 const Chat = () => {
-  return (
-    <div className='chat'>
-      
-    </div>
-  )
+    return (
+        <div className='chat'>
+            <div className='chatInfo'>
+                <span>Jane</span>
+                <div className='chatIcons'>
+                    <img src={Cam} alt='' />
+                    <img src={Add} alt='' />
+                    <img src={More} alt='' />
+                </div>
+            </div>
+            <Messages />
+            <Input />
+        </div>
+    )
 }
 
 export default Chat
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='navbar'>
-    <span className='logo'>Lama chat</span>
+    <span className='logo'>Lama Chat</span>
     <div className='user'>
     <img src='https://images.pexels.com/photos/15488486/pexels-photo-15488486/free-photo-of-a-black-and-white-photo-of-a-smiling-boy.jpeg?auto=compress&cs=tinysrgb&w=600' alt=''/>
     <span>John</span>
