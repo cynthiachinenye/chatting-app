18824cac3b44a1baa827e313826e65beefe8b71f
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-28 14:44:42 +0100

    new

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const Message = () => {
-  return (
-    <div className='message'>
-      
-    </div>
-  )
+    return (
+        <div className='message owner'>
+            <div className="messageInfo">
+                <img src='https://images.pexels.com/photos/19153152/pexels-photo-19153152/free-photo-of-a-person-standing-on-top-of-a-rock-in-the-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
+            </div>
+            <div className='messageContent'>
+            <p>hello</p>
+            <img src='https://images.pexels.com/photos/19153152/pexels-photo-19153152/free-photo-of-a-person-standing-on-top-of-a-rock-in-the-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
+          
+            </div>
+
+        </div>
+    )
 }
 
 export default Message
---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvmixjube3PKyHBz6pSLnVsk-nN1Yh7vU",
+  authDomain: "chatapp-6bf41.firebaseapp.com",
+  projectId: "chatapp-6bf41",
+  storageBucket: "chatapp-6bf41.appspot.com",
+  messagingSenderId: "115235085228",
+  appId: "1:115235085228:web:2a865fbde920595b00b809"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
