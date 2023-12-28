8e991ccc231662d60d145d9916144e9586969703
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-28 11:37:27 +0100

    new

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,7 +4,19 @@ const Register = () => {
   return (
     <div className='formContainer'>
 
-    <div className='formWrapper'></div>
+    <div className='formWrapper'>
+    <span className='logo'>Lama Chat</span>
+    <span className='title'>Register</span>
+    <form>
+    <input type='text' placeholder="display name"/>
+    <input type='email' placeholder='email'/>
+    <input type='password' placeholder="password"/>
+    <input type='file'/>
+    <button>Sign up</button>
+    </form>
+    <p>do you have an account? Login</p>
+    
+    </div>
       
     </div>
   )
