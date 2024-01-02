3a1b9ffb0860ceab2f4a2691b4dd632257c49741
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-02 15:27:43 +0100

    new

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
-import { collection, query, where, getDocs } from "firebase/firestore"
+import React, { useContext, useState } from 'react'
+import { collection, query, where, getDocs, updateDoc, serverTimestamp,setDoc,doc } from "firebase/firestore"
 import {db} from "../Firebase"
+import {AuthContext} from "../Components/context/AuthContext"
 const Search = () => {
     const [username, setUsername] = useState("")
     const [user, setUser] = useState(null)
     const [err, setErr] = useState(false)
 
+    const {currentUser} = useContext(AuthContext)
+
     const handleSubmit =  async () =>{
-        const q = query(collection(db,"users"), where("displayName", "==", "username"));
+        const q = query(collection(db,"users"), 
+        where("displayName", "==", username) );
 
     
     try{
@@ -15,22 +19,49 @@ const Search = () => {
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
      setUser(doc.data());
-      console.log(doc.id, " => ", doc.data());
+ 
     });
 }catch(err){
- setErr(true)
+ setErr(true);
 }
 }
     const handleKey = e => {
-        e.code=== "Enter" && handleSubmit()
+        e.code === "Enter" && handleSubmit()
+    }
+    const handleSelect = async() =>{
+        // check whether the group(vhats in firestore) exists. if not
+          const combinedId = currentUser.uid > user.uid 
+          ? currentUser.uid + currentUser.uid
+           : user.uid + currentUser.uid;
+          try{
+          const res = await getDocs(db,"chats", combinedId); 
+
+           if(!res.exists()){
+            // create a chat in chats collection
+            await setDoc(doc,(db,"chats", combinedId),{messages:[] });
+             
+            // create user chats
+           await updateDoc(doc(db, "userChats", currentUser.uid),{
+            [combinedId + ".userInfo"]:{
+                uid:user.uid,
+                displayName:user.displayName,
+                photoURL:user.photoURL
+            },
+            [combinedId +".date"]:serverTimestamp()
+           })
+        }
+        }catch(err){
+
+          }
+      
     }
     return ( 
         <div className='search'>
             <div className='searchForm'>
                 <input type='text' placeholder='Find a user'  onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
             </div>
-          {err && <span>User not found </span>}
-        {user && <div className='userChat'>
+          {err && <span>User not found! </span>}
+        {user && <div className='userChat' onClick={handleSelect}>
             <img src={user.photoURL} alt='' />
             <div className='userChatInfo'>
                 <span>{user.displayName}</span>
