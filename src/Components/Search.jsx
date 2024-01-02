ec17bb195768531dcb2d57130c6e567836032bdb
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-02 01:19:00 +0100

    new

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
-    return (
+    const [username, setUsername] = useState("")
+    const [user, setUser] = useState(null)
+    const [err, setErr] = useState(false)
+    const handleSubmit = e =>{}
+    const handleKey = e => {
+        e.code=== "Enter" && handleSubmit()
+    }
+    return ( 
         <div className='search'>
             <div className='searchForm'>
-                <input type='text' placeholder='Find a user'/>
+                <input type='text' placeholder='Find a user'  onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
             </div>
             <div className='userChat'>
                 <img src='https://images.pexels.com/photos/19153152/pexels-photo-19153152/free-photo-of-a-person-standing-on-top-of-a-rock-in-the-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='' />
